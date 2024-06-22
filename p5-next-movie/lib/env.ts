fc521f3673e6b3a9e1334419a15dd4d4a854778a
Italo Castillo <italogitdoe@gmail.com>
2024-06-22 18:42:37 +0200

    add env function

---
+++ p5-next-movie/lib/env.ts
@@ -0,0 +1,8 @@
+export const env = (varname: string) => {
+    const value = process.env[varname];
+    if (value === undefined) {
+      throw new Error(`Missing env variable ${varname} (:|`);
+    }
+    return value;
+  };
+  
