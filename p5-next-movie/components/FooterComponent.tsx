21413d24ce8f0c22ceb4253663c4b7cd9c4ec90d
Italo Castillo <italogitdoe@gmail.com>
2024-06-23 01:49:28 +0200

    update footer

--- p5-next-movie/components/FooterComponent.tsx
+++ p5-next-movie/components/FooterComponent.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full flex flex-row items-center justify-center h-10 bg-slate-50  z-10 backdrop-blur-md bg-slate-600/30">
-      <div>footer</div>
+      <div>No rights reserved</div>
     </footer>
   );
 }
