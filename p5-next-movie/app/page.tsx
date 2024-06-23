213e38b97e75ae3bedfc2d3a3006a417dfb87158
Italo Castillo <italogitdoe@gmail.com>
2024-06-23 04:23:11 +0200

    refactoring . create category obkect

--- p5-next-movie/app/page.tsx
+++ p5-next-movie/app/page.tsx
@@ -11,6 +11,7 @@ import CarouselHzComponent from "@/components/CarouselHzComponent";
 import CarouselPopularComponent from "@/components/CarouselPopularComponent";
 import CarouselPopularPeopleComponent from "@/components/CarouselPopularPeopleComponent";
 import CaptionComponent from "@/components/CaptionComponent";
+import { movieCategories } from "@/lib/utils";
 
 export default async function Home() {
   const now_playing_movies = await actionGetMoviesNowPlaying();
@@ -21,7 +22,6 @@ export default async function Home() {
   const from_90s = await actionGetMoviesLteGte("1999-12-12", "1990-01-01");
   const from_80s = await actionGetMoviesLteGte("1990-01-01", "1980-01-01");
   const people = await actionGetPeopleTopRated();
-
   return (
     <main className="flex-1  md:container mx-auto    min-h-screen">
       <div className="w-full flex flex-row h-auto items-center justify-center gap-10">
@@ -30,9 +30,11 @@ export default async function Home() {
         </div>
 
         <div className="w-1/4 hidden md:flex flex-col ">
-          <div className="relative -top-16 caption">    
-           <CaptionComponent caption={"Upcoming"} href={"/movies/upcoming"}></CaptionComponent>
-
+          <div className="relative -top-16 caption">
+            <CaptionComponent
+              caption={movieCategories.upcoming.text}
+              href={movieCategories.upcoming.href}
+            ></CaptionComponent>
           </div>
           <CarouselHzComponent movies={upcoming_movies}></CarouselHzComponent>
         </div>
@@ -41,7 +43,10 @@ export default async function Home() {
         <div>
           {/* Popular */}
           <div>
-            <CaptionComponent caption={"Popular"} href={"/movies/popular"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.popular.text}
+              href={movieCategories.popular.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularComponent
                 movies={popular}
@@ -51,7 +56,10 @@ export default async function Home() {
 
           {/* Top Rated */}
           <div>
-            <CaptionComponent caption={"Top Rated"} href={"/movies/top-rated"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.top_rated.text}
+              href={movieCategories.top_rated.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularComponent
                 movies={top_rated}
@@ -61,7 +69,10 @@ export default async function Home() {
 
           {/* From 2000's */}
           <div>
-            <CaptionComponent caption={"From 2000's"} href={"/movies/2000"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.from_2000.text}
+              href={movieCategories.from_2000.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularComponent
                 movies={from_2000s}
@@ -71,7 +82,10 @@ export default async function Home() {
 
           {/* From 90's */}
           <div>
-            <CaptionComponent caption={"From 90's"} href={"/movies/1990"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.from_1990.text}
+              href={movieCategories.from_1990.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularComponent
                 movies={from_90s}
@@ -81,7 +95,10 @@ export default async function Home() {
 
           {/* From 80's */}
           <div>
-            <CaptionComponent caption={"From 80's"} href={"/movies/1980"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.from_1980.text}
+              href={movieCategories.from_1980.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularComponent
                 movies={from_80s}
@@ -91,7 +108,10 @@ export default async function Home() {
 
           {/* People */}
           <div>
-            <CaptionComponent caption={"People"} href={"/people/popular"}></CaptionComponent>
+            <CaptionComponent
+              caption={movieCategories.people.text}
+              href={movieCategories.people.href}
+            ></CaptionComponent>
             <div>
               <CarouselPopularPeopleComponent
                 people={people}
--- p5-next-movie/lib/utils.ts
+++ p5-next-movie/lib/utils.ts
@@ -1,11 +1,60 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { MovieCategories } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 export const getMoviePicture = (imageId: string) => {
   return `https://image.tmdb.org/t/p/original/${imageId}`;
+};
+export const getDefaultMoviePicture = () => {
+  return "/default.webp";
+};
+
+export const getDefaultDateFormat = () => {
+  var dateOptions = { year: "numeric", month: "long", day: "numeric" };
+};
+
+export function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const movieCategories: MovieCategories = {
+  upcoming: {
+    code: "upcoming",
+    text: "Upcoming",
+    href: "/movies/upcoming",
+  },
+  popular: {
+    code: "popular",
+    text: "Popular",
+    href: "/movies/popular",
+  },
+  top_rated: {
+    code: "top-rated",
+    text: "Top Rated",
+    href: "/movies/top-rated",
+  },
+  from_2000: {
+    code: "from-2000",
+    text: "From 2000",
+    href: "/movies/from-2000",
+  },
+  from_1990: {
+    code: "from-1990",
+    text: "From 1990",
+    href: "/movies/from-1990",
+  },
+  from_1980: {
+    code: "from-1980",
+    text: "From 1980",
+    href: "/movies/from-1980",
+  },
+  people: {
+    code: "people",
+    text: "People",
+    href: "/movies/people",
+  },
+};
