c6d02443bb9119ddccc178a1e7164f93de61ae65
Italo Castillo <italogitdoe@gmail.com>
2024-06-23 04:22:02 +0200

    add interface for category

--- p5-next-movie/lib/types.ts
+++ p5-next-movie/lib/types.ts
@@ -8,7 +8,7 @@ export type Movie = {
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: string;
+  release_date: string | Date;
   title: string;
   video: boolean;
   vote_average: number;
@@ -45,3 +45,19 @@ export type PersonResult = {
     total_pages: number;
     total_results: number;
 };
+
+export interface Category {
+  text: string;
+  href: string;
+  code?: string; // optional since not all categories have a code
+}
+
+export interface MovieCategories {
+  upcoming: Category;
+  popular: Category;
+  top_rated: Category;
+  from_2000: Category;
+  from_1990: Category;
+  from_1980: Category;
+  people: Category;
+}
